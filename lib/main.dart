1b6f53d89f2702eafcdd3a5974f1258a89094911
Janaka Walakuluarachchi <walakulu@gmail.com>
2022-10-11 21:43:42 +0530

    UI created

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.blue.shade900,
-          title: Text('Ask Me Anything!'),
-        ),
-        body: MagicQuiz(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything!'),
       ),
-    )
-  );
+      body: MagicQuiz(),
+    ),
+  ));
 }
 
 class MagicQuiz extends StatefulWidget {
+  const MagicQuiz({Key? key}) : super(key: key);
+
   @override
   State<MagicQuiz> createState() => _MagicQuizState();
 }
 
 class _MagicQuizState extends State<MagicQuiz> {
 
-  int magicBall = 2;
-  
+  int ballNumber = 3;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-              child: Image.asset('images/ball$magicBall.png'),),
+            child: TextButton(
+              onPressed: (){
+                ballNumber = 1;
+                print('I got clicked');
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
